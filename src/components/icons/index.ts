6342bb5c74994c556bad99f063e024156b6d4629
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-06-07 20:28:50 -0300

    feat: add icons components

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as CheckIcon } from "./check-icon";
+export { default as ChevronDownIcon } from "./chevron-down-icon";
+export { default as ChevronUpIcon } from "./chevron-up-icon";
