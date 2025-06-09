34ca3b3aff2b8b5c94fd6a51dafc48dd9f69529c
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-06-09 02:09:35 -0300

    feat: add sonner and dialog components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { SymbolsProvider } from "@/providers/symbols-provider";
+import { Toaster } from "@/components/ui/sonner";
 import App from "@/App";
 
 import "@/styles/tailwind.css";
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SymbolsProvider>
       <App />
+      <Toaster richColors duration={3000} closeButton />
     </SymbolsProvider>
   </StrictMode>,
 );
